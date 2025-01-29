361c60da418697716e560af28675959bf2e3a99a
Philip-Matthew <philipmatthew26@gmail.com>
2025-01-29 10:31:21 +0530

    .env config

--- e_commerce_project/settings.py
+++ e_commerce_project/settings.py
@@ -16,6 +16,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,7 +86,6 @@ load_dotenv()
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 
 
 # Password validation
